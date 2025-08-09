f1d38b75f4d13f98b2f4fdf02da088c1a6776fb4
zMr-Profesor4556 <nipunofficial4@gmail.com>
2025-08-09 19:33:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,13 +27,13 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/5rujjj.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/2d7onp.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "AKASH-MD-V4",
+BOT_NAME: process.env.BOT_NAME || "N-TECH",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "AKASH-MD-V4",
+STICKER_NAME: process.env.STICKER_NAME || "N-TECH OFFICIAL🇱🇰",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -41,15 +41,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94753686350",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94729007640",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "*Akash-MD Official*",
+OWNER_NAME: process.env.OWNER_NAME || "𝐌𝖗 𝐍𝖎𝖕𝖚𝖓 𝐎𝖋𝖈ᴹᴴᵀ",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Akash-MD Official ❣️*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/5rujjj.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2d7onp.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar ⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> 𝐌𝖗 𝐍𝖎𝖕𝖚𝖓 𝐎𝖋𝖈ᴹᴴᵀ",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -73,7 +73,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "94753686350",
+DEV: process.env.DEV || "94729007640",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
